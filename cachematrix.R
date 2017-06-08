668e336a98d017b6631a9da0060288f9886813c2
Lieve Lanoye <lieve.lanoye@tpv-tech.com>
2017-06-08 23:10:32 +0200

    Assignment2 of Programming in R
    2 functions were created to allow caching of an inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix and cacheSolve work together to return the inverse of a Matrix, either by checking the cache 
+## or if it was not cached yet, by calculating the inverse
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special vector of a given matrix that needs inverting with functions 
+## to handle matrix caching, by allowing to set and get the matrix and by allowing to set  
+## and get the inverse, regardless of the environment.
+makeCacheMatrix <- function(x = matrix(1:1)) { # x is a matrix, but matrix needs at least the data element, so I put a default
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function takes the special vector produced by makeChacheMatrix and gets the inverse. If that is not existing, 
+## it calculates the inverse and stores it using the setinverse function of makeChacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()                         
+  if(!is.null(inv)) {                           #check if inverse was cached
+    message("getting cached data")
+    return(inv) 
+  }
+  data <- x$get()
+  inv <- solve(data, diag(dim(data)[1]), ...)   #diag(dim(data)[1]) creates unity matrix with same dimension as data, forcing "solve" to revert the inverse
+  x$setinverse(inv)
+  inv
 }
